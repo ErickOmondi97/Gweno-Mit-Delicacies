a971ae92053689faf680fb9556f444e495297950
Erick Omondi <eriqueasaria@gmail.com>
2023-09-06 17:58:23 +0300

    I added code to make the left arrow appear or dissappear depending on the user's actions

--- js/main.js
+++ js/main.js
@@ -5,8 +5,15 @@ let right_arrow = document.getElementById("right_arrow");
 
 right_arrow.addEventListener('click', () => {
   menu_sec.scrollLeft += 115;
+
+  if(menu_sec.scrollLeft > 115) {
+    right_arrow.style.visibility = "hidden";
+    console.log("yes, it works")
+  }
 })
 
 left_arrow.addEventListener('click', () => {
   menu_sec.scrollLeft -= 115;
+
+  right_arrow.style.visibility = "visible";
 })
