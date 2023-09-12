73b80156b987f9f8cfa3a52b49ac534d7a595f4a
Erick Omondi <eriqueasaria@gmail.com>
2023-09-12 15:46:35 +0300

    I added code for making the js video play and pause

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-//VARIABLES
+/* MENU START SECTION */
 let menu_sec =document.getElementById("menu_sec");
 let left_arrow = document.getElementById("left_arrow");
 let right_arrow = document.getElementById("right_arrow");
@@ -10,10 +10,36 @@ right_arrow.addEventListener('click', () => {
     right_arrow.style.visibility = "hidden";
     console.log("yes, it works")
   }
-})
+});
 
 left_arrow.addEventListener('click', () => {
   menu_sec.scrollLeft -= 115;
 
   right_arrow.style.visibility = "visible";
+});
+
+/* PAUSE/PLAY BUTTON */
+let videos = document.getElementById('video')[0];
+let plays = document.getElementById('play')[0];
+let pauses = document.getElementById('pause')[0];
+let screens = document.getElementById('screen')[0];
+
+plays.addEventListener('click', ()=> {
+  videos.play();
+  plays.style.display = "none";
+  screens.style.display = "none";
+  pauses.style.display = "flex";
+});
+
+pauses.addEventListener('click', () => {
+  videos.pause();
+  plays.style.display = "flex";
+  screens.style.display = "flex";
+  pauses.style.display = "none";
+})
+
+videos.addEventListener('ended', () => {
+  plays.style.display = "flex";
+  screens.style.display = "flex";
+  pauses.style.display = "none";
 })
