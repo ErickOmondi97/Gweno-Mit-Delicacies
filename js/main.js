f51db9986d2b4f4605879dfcd427dd12465077e0
Erick Omondi <eriqueasaria@gmail.com>
2023-09-16 12:49:46 +0300

    I added the setTimeout and setInterval for the dots

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ left_arrow.addEventListener('click', () => {
 });
 
 /* PAUSE/PLAY BUTTON */
-let videos = document.getElementById('video')[0];
+/* let videos = document.getElementById('video')[0];
 let plays = document.getElementById('play')[0];
 let pauses = document.getElementById('pause')[0];
 let screens = document.getElementById('screen')[0];
@@ -42,4 +42,48 @@ videos.addEventListener('ended', () => {
   plays.style.display = "flex";
   screens.style.display = "flex";
   pauses.style.display = "none";
-})
+}) */
+
+/* HEADER SHIFT */
+const dataload = () => {
+  const sec_title = document.getElementById('sec_title');
+  const h6_dots = document.getElementsByTagName('h6');
+
+  setTimeout(() => {
+    sec_title.innerHTML = "BARGAIN <br> BUCKET MENU <br> CAMPAIGN"
+    h6_dots[1].classList.add("head_dots_main");
+    h6_dots[2].classList.remove("head_dots_main");
+    h6_dots[3].classList.remove("head_dots_main");
+    h6_dots[4].classList.remove("head_dots_main");
+  }, 0000);
+
+  setTimeout (() => {
+    sec_title.innerHTML = "BURGER <br> MENU <br> CAMPAIGN"
+    h6_dots[1].classList.remove("head_dots_main");
+    h6_dots[2].classList.add("head_dots_main");
+    h6_dots[3].classList.remove("head_dots_main");
+    h6_dots[4].classList.remove("head_dots_main");
+  }, 3000);
+  
+  setTimeout (() => {
+    sec_title.innerHTML = "BARGAIN <br> FRIES MENU <br> CAMPAIGN"
+    h6_dots[1].classList.remove("head_dots_main");
+    h6_dots[2].classList.remove("head_dots_main");
+    h6_dots[3].classList.add("head_dots_main");
+    h6_dots[4].classList.remove("head_dots_main");
+  }, 6000);
+  
+  setTimeout (() => {
+    sec_title.innerHTML = "BARGAIN <br> CHICKEN MENU <br> CAMPAIGN"
+    h6_dots[1].classList.remove("head_dots_main");
+    h6_dots[2].classList.remove("head_dots_main");
+    h6_dots[3].classList.remove("head_dots_main");
+    h6_dots[4].classList.add("head_dots_main");
+  }, 9000);
+
+  console.log("I can see this");
+};
+
+setInterval(dataload, 12000);
+
+dataload();
